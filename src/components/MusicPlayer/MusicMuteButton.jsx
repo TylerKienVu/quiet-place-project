f582dcea50c9abeb2c236aef889f1bbfc5a67f21
Tyler Vu <tylerkienvu@gmail.com>
2020-01-29 08:25:02 -0800

    got rid of parent encapsulating folder

---
+++ src/components/MusicPlayer/MusicMuteButton.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp, faVolumeMute } from "@fortawesome/free-solid-svg-icons";
+import "./MusicMuteButton.css";
+
+const MusicMuteButton = props => {
+  const { audio } = props;
+  const [audioIsOn, setAudioIsOn] = useState(true);
+
+  useEffect(() => {
+    audioIsOn ? audio.play() : audio.pause();
+  }, [audioIsOn, audio]);
+
+  const toggleMusic = () => {
+    setAudioIsOn(!audioIsOn);
+  };
+
+  return (
+    <div
+      className='MusicMuteButton-container d-flex justify-content-center align-items-center'
+      onClick={toggleMusic}
+    >
+      <FontAwesomeIcon icon={audioIsOn ? faVolumeUp : faVolumeMute} />
+    </div>
+  );
+};
+
+export default MusicMuteButton;
