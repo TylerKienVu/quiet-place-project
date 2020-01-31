8545d31a398d125a405015cfc186507da8263634
Tyler Vu <tylerkienvu@gmail.com>
2020-01-31 00:35:22 -0800

    Added check to make sure audio is playing

--- src/components/MusicPlayer/MusicMuteButton.jsx
+++ src/components/MusicPlayer/MusicMuteButton.jsx
@@ -12,6 +12,12 @@ const MusicMuteButton = props => {
   }, [audioIsOn, audio]);
 
   const toggleMusic = () => {
+    // Make sure that the audio is playing. Sometimes it doesn't play automatically
+    // even though autoplay is enabled. Sometimes play() can fail if the user
+    // didn't interact with the document so placing it here will ensure that they
+    // did by pressing the volume button.
+    if (audio.paused) audio.play();
+
     setAudioIsOn(!audioIsOn);
   };
 
