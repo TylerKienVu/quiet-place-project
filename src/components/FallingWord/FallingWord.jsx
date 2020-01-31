52afeef5d91384c2f16f916b435af92c9ed9ff29
Tyler Vu <tylerkienvu@gmail.com>
2020-01-31 00:40:34 -0800

    Implemented crumbles

--- src/components/FallingWord/FallingWord.jsx
+++ src/components/FallingWord/FallingWord.jsx
@@ -1,35 +1,85 @@
 import React, { useRef, useState, useEffect, useCallback } from "react";
 import "./FallingWord.css";
-import { randomNumberInRange } from "../../util/generalUtils";
+import { randomNumberInRange, randomUniqueNumbersInRange } from "../../util/general";
 
-const fallingAnimationClassNames = ["FallingWord-falling1", "FallingWord-falling2"];
+const fallingWordAnimationClassNames = ["FallingWord-word-falling1", "FallingWord-word-falling2"];
+const fallingCrumbleAnimationClassNames = [
+  "FallingWord-crumble-falling3",
+  "FallingWord-crumble-falling4",
+  "FallingWord-crumble-falling5",
+  "FallingWord-crumble-falling6"
+];
+const MIN_NUM_OF_CRUMBLES = 1;
+const MAX_NUM_OF_CRUMBLES = 3;
 
 const FallingWord = props => {
-  const [startingPosition, setStartingPosition] = useState(null);
-  const [animationClass, setAnimationClass] = useState(null);
-  const measuringContainerRef = useRef(null);
   const { word, id, offsetWidth, deleteFallingWord } = props;
 
-  const calculateOffset = useCallback(() => {
-    const wordWidth = measuringContainerRef.current.offsetWidth;
+  const [animationClass, setAnimationClass] = useState(null);
+  const [crumblesAnimationClasses, setCrumblesAnimationClasses] = useState([]);
+  const [startingPosition, setStartingPosition] = useState(null);
+  const measuringContainerRef = useRef(null);
 
-    return offsetWidth - wordWidth;
+  // Calculates the starting position for the falling nodes
+  const calculateOffset = useCallback(() => {
+    return measuringContainerRef.current
+      ? offsetWidth - measuringContainerRef.current.offsetWidth
+      : null;
   }, [offsetWidth]);
 
-  // When the user types, it causes a rerener of all of the currently falling words.
+  // Creates a list of crumble nodes to render
+  // Note: Had to generate the list of components before render because when I was
+  // trying to map the components in render, the crumbles would constantly re-render when
+  // the user typed a character. This may be a fundamental issue with how a character
+  // being typed causes a re-render
+  const generateCrumbleNodes = useCallback(
+    crumbleAnimations => {
+      return crumbleAnimations.map((animationClass, index) => (
+        <span
+          className={`FallingWord-falling ${animationClass}`}
+          key={`${animationClass}-${index}`}
+          style={{ left: startingPosition }}
+        >
+          .
+        </span>
+      ));
+    },
+    [startingPosition]
+  );
+
+  // Generates a random number of unique crumble animations between 2-3
+  const generateRandomCrumbleAnimationClasses = useCallback(() => {
+    // generate MIN_NUM_OF_CRUMBLES to MAX_NUM_OF_CRUMBLES unique animation indicies
+    const crumbleAnimationIndicies = randomUniqueNumbersInRange(
+      0,
+      fallingCrumbleAnimationClassNames.length - 1,
+      MIN_NUM_OF_CRUMBLES,
+      MAX_NUM_OF_CRUMBLES
+    );
+
+    // Get the corresponding animations
+    const crumbleAnimations = crumbleAnimationIndicies.map(
+      crumbleAnimationIndex => fallingCrumbleAnimationClassNames[crumbleAnimationIndex]
+    );
+
+    return generateCrumbleNodes(crumbleAnimations);
+  }, [generateCrumbleNodes]);
+
+  // When the user types, it causes a rerender of all of the currently falling words.
   // The code inside here is only meant to be executed once.
   useEffect(() => {
     setStartingPosition(calculateOffset());
+    setCrumblesAnimationClasses(generateRandomCrumbleAnimationClasses());
     setAnimationClass(getRandomFallingAnimationClass());
-  }, [calculateOffset]);
+  }, [calculateOffset, generateRandomCrumbleAnimationClasses]);
 
   const handleAnimationEnd = () => {
     deleteFallingWord(id);
   };
 
   const getRandomFallingAnimationClass = () => {
-    return fallingAnimationClassNames[
-      randomNumberInRange(0, fallingAnimationClassNames.length - 1)
+    return fallingWordAnimationClassNames[
+      randomNumberInRange(0, fallingWordAnimationClassNames.length - 1)
     ];
   };
 
@@ -39,12 +89,13 @@ const FallingWord = props => {
         {word}
       </div>
       <span
-        className={`FallingWord-falling-word ${animationClass}`}
+        className={`FallingWord-falling ${animationClass}`}
         style={{ left: startingPosition }}
         onAnimationEnd={() => handleAnimationEnd()}
       >
         {word}
       </span>
+      {crumblesAnimationClasses}
     </>
   );
 };
