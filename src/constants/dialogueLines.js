10a6a082421508d6cba5ddf9207aa59877c5a623
Tyler Vu <tylerkienvu@gmail.com>
2020-02-02 22:59:33 -0800

    Added a constant file containing dialogue lines

---
+++ src/constants/dialogueLines.js
@@ -0,0 +1,41 @@
+import React from "react";
+import IntroDialogueFallingWord from "../components/IntroDialogue/IntroDialogueFallingWord";
+
+const DialogueLines = [
+  <>
+    <div>communication with the thoughts room will be operated via [spacebar] key.</div>
+    <div>(gently squeeze that key, now).</div>
+  </>,
+  <>
+    <div>
+      in order to get the full experience, please silence your phone(s) and turn on your sound.
+    </div>
+    <div>(again, [spacebar] to carry on.)</div>
+  </>,
+  <div>welcome to the thoughts room.</div>,
+  <div>so...</div>,
+  <div>...you chose quiet.</div>,
+  <div>but what can you do in your quiet place?</div>,
+  <div>what can you do with all of these thoughts that are just...</div>,
+  <IntroDialogueFallingWord />,
+  <div>well, look up there. this is your thoughts box.</div>,
+  <div>
+    it acts just like the status box you are familiar with from your favorite social network.
+  </div>,
+  <div>really, type something into the box and press the [enter] key</div>,
+  <div>very good ;)</div>,
+  <div>in a few seconds, your thoughts box will be laid in front of you again.</div>,
+  <div>and I want you to pour all of your burdens and heart-aches into it.</div>,
+  <div>don't worry. no one will be able to view, comment on, or judge you on your thoughts.</div>,
+  <>
+    <div>
+      in fact, your thoughts will disappear by literally bursting into thousands of stars so you can
+      finally find some rest.
+    </div>
+    <div>(i swear, just watch the screen while you type)</div>
+  </>,
+  <div>are you ready?</div>,
+  <div>here we go :)</div>
+];
+
+export default DialogueLines;
