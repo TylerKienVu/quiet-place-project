fc73dbcbecb9bc132851298b3a9232b96e334920
Tyler Vu <tylerkienvu@gmail.com>
2020-02-02 22:59:46 -0800

    Moved the animation constants to a constant file

---
+++ src/constants/animationClassNames.js
@@ -0,0 +1,11 @@
+export const FallingWordAnimationClassNames = [
+  "FallingWord-word-falling1",
+  "FallingWord-word-falling2"
+];
+
+export const FallingCrumbleAnimationClassNames = [
+  "FallingWord-crumble-falling3",
+  "FallingWord-crumble-falling4",
+  "FallingWord-crumble-falling5",
+  "FallingWord-crumble-falling6"
+];
