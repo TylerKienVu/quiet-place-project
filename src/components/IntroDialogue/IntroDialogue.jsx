43990a578a71ac2f9d5e901ce3c170dc333611b9
Tyler Vu <tylerkienvu@gmail.com>
2020-02-03 09:01:47 -0800

    Added more state handling for intro dialogue

--- src/components/IntroDialogue/IntroDialogue.jsx
+++ src/components/IntroDialogue/IntroDialogue.jsx
@@ -4,12 +4,15 @@ import { CSSTransition } from "react-transition-group";
 import "./IntroDialogue.css";
 
 const LINE_INDEX_TO_SHOW_THOUGHTBOX = 8;
+const LINE_INDEX_USER_IS_TYPING_INTO_THOUGHTBOX = 10;
+const LINE_INDEX_TO_HIDE_THOUGHTBOX = 11;
+const LINE_INDEX_TO_HIDE_DIALOGUE = DialogueLines.length;
 
 const IntroDialogue = props => {
-  const { setShowThoughtBox, toggleMusic, toggleThoughtBox } = props;
+  const { toggleThoughtBox, setAudioIsOn, toggleDialogue, toggleFocusThoughtBox } = props;
   const [currentDialogueLineIndex, setCurrentDialogueLineIndex] = useState(0);
-
   const [showLine, setShowLine] = useState(false);
+  const [userTypedSomethingIntoThoughtBox, setUserTypedSomethingIntoThoughtBox] = useState(false);
 
   useEffect(() => {
     document.addEventListener("keydown", handleKeyDown);
@@ -24,22 +27,33 @@ const IntroDialogue = props => {
   }, [currentDialogueLineIndex]);
 
   const handleKeyDown = keyDownEvent => {
+    // Dont show next line until user types something in thoughtbox and presses enter here
+    if (currentDialogueLineIndex === LINE_INDEX_USER_IS_TYPING_INTO_THOUGHTBOX) {
+      if (keyDownEvent.code !== "Enter" && keyDownEvent.code !== "Space")
+        setUserTypedSomethingIntoThoughtBox(true);
+      if (keyDownEvent.code === "Enter" && userTypedSomethingIntoThoughtBox) setShowLine(false);
+      return;
+    }
+
     // Hide the line to trigger exit animation
     if (keyDownEvent.code === "Space") {
       setShowLine(false);
-      if (currentDialogueLineIndex === 0) toggleMusic();
+      if (currentDialogueLineIndex === 0) setAudioIsOn(true);
     }
   };
 
   const handleLineExitedTransitionFinished = () => {
+    const nextLineIndex = currentDialogueLineIndex + 1;
+
     // Activate next line
     setCurrentDialogueLineIndex(prevIndex => prevIndex + 1);
 
-    if (
-      currentDialogueLineIndex + 1 === LINE_INDEX_TO_SHOW_THOUGHTBOX ||
-      currentDialogueLineIndex + 1 === DialogueLines.length
-    )
-      setShowThoughtBox(true);
+    if (nextLineIndex === LINE_INDEX_TO_SHOW_THOUGHTBOX || nextLineIndex === DialogueLines.length)
+      toggleThoughtBox();
+    else if (nextLineIndex === LINE_INDEX_TO_HIDE_THOUGHTBOX) toggleThoughtBox();
+
+    if (nextLineIndex === LINE_INDEX_USER_IS_TYPING_INTO_THOUGHTBOX) toggleFocusThoughtBox();
+    if (nextLineIndex === LINE_INDEX_TO_HIDE_DIALOGUE) toggleDialogue();
   };
 
   return (
