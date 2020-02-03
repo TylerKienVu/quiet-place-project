9181b4b49f1675465506d5a485c5e2c02cd3651a
Tyler Vu <tylerkienvu@gmail.com>
2020-02-03 09:02:11 -0800

    Added togglers for state

--- src/components/MainApp/App.jsx
+++ src/components/MainApp/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import "./App.css";
 import ThoughtBox from "../ThoughtBox/ThoughtBox";
 import IntroDialogue from "../IntroDialogue/IntroDialogue";
 import soundtrack from "../../assets/quietPlaceProjectSoundtrack.mp3";
 import MusicMuteButton from "../MusicMuteButton/MusicMuteButton";
 import { CSSTransition } from "react-transition-group";
+import ToggleDialogueButton from "../ToggleDialogueButton/ToggleDialogueButton";
 
 const audio = new Audio(soundtrack);
 audio.loop = true;
@@ -15,20 +16,38 @@ function App() {
   const [showThoughtBox, setShowThoughtBox] = useState(false);
   const [showDialogue, setShowDialogue] = useState(true);
   const [audioIsOn, setAudioIsOn] = useState(false);
+  const [focusThoughtBox, setFocusThoughtBox] = useState(false);
 
-  const toggleMusic = () => {
+  useEffect(() => {
     // Make sure that the audio is playing. Sometimes it doesn't play automatically
     // even though autoplay is enabled. Sometimes play() can fail if the user
     // didn't interact with the document so placing it here will ensure that they
     // did by pressing the volume button.
-    if (audio.paused) audio.play();
 
-    setAudioIsOn(!audioIsOn);
-  };
+    if (audio.paused && audioIsOn) audio.play();
+  }, [audioIsOn]);
 
-  const toggleThoughtBox = () => {
-    setShowThoughtBox(!showThoughtBox);
-  };
+  const toggleMusic = useCallback(() => {
+    setAudioIsOn(prevVal => !prevVal);
+  }, []);
+
+  const toggleThoughtBox = useCallback(() => {
+    setShowThoughtBox(preVal => !preVal);
+  }, []);
+
+  const toggleFocusThoughtBox = useCallback(() => {
+    setFocusThoughtBox(prevVal => !prevVal);
+  }, []);
+
+  const toggleDialogue = useCallback(() => {
+    if (showDialogue) {
+      setShowDialogue(false);
+      setShowThoughtBox(true);
+    } else {
+      setShowDialogue(true);
+      setShowThoughtBox(false);
+    }
+  }, [showDialogue]);
 
   return (
     <div className='container-fluid App-main-container p-0'>
@@ -39,16 +58,20 @@ function App() {
         classNames='App-ThoughtBox'
         unmountOnExit
       >
-        <ThoughtBox />
+        <ThoughtBox focusThoughtBox={focusThoughtBox} />
       </CSSTransition>
       {showDialogue && (
         <IntroDialogue
-          setShowThoughtBox={setShowThoughtBox}
-          toggleMusic={toggleMusic}
+          setAudioIsOn={setAudioIsOn}
+          toggleDialogue={toggleDialogue}
           toggleThoughtBox={toggleThoughtBox}
+          toggleFocusThoughtBox={toggleFocusThoughtBox}
         />
       )}
-      <MusicMuteButton audio={audio} audioIsOn={audioIsOn} toggleMusic={toggleMusic} />
+      <div className='App-footer d-flex flex-row'>
+        <ToggleDialogueButton toggleDialogue={toggleDialogue} showDialogue={showDialogue} />
+        <MusicMuteButton audio={audio} audioIsOn={audioIsOn} toggleMusic={toggleMusic} />
+      </div>
     </div>
   );
 }
